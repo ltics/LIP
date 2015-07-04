eabe36840f74caa171c6e36fae7a02be461628d1
zjhmale <zjhmale@gmail.com>
2015-07-04 12:46:21 +0800

    add dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.antlr/antlr "3.2"]
+                 [org.antlr/stringtemplate "3.2.1"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
